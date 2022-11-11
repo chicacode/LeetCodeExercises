0e56518d299a0b81d4f7c38071cba6f7a45c2a27
chicacode <gerahernan.al@gmail.com>
2022-11-10 18:20:14 -0800

    feat: adding student record project

---
+++ src/Student_Store_Record/src/dto/Student.java
@@ -0,0 +1,64 @@
+package dto;
+
+public class Student {
+    private int Id;
+    private String Name;
+    private String Email;
+    private String StudentId;
+    private int StudentRollNumber;
+
+
+    public Student() {
+    }
+
+
+    public Student(int Id, String Name, String Email, String StudentId, int StudentRollNumber) {
+        this.Id = Id;
+        this.Name = Name;
+        this.Email = Email;
+        this.StudentId = StudentId;
+        this.StudentRollNumber = StudentRollNumber;
+    }
+
+
+    public int getId() {
+        return this.Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getEmail() {
+        return this.Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getStudentId() {
+        return this.StudentId;
+    }
+
+    public void setStudentId(String StudentId) {
+        this.StudentId = StudentId;
+    }
+
+    public int getStudentRollNumber() {
+        return this.StudentRollNumber;
+    }
+
+    public void setStudentRollNumber(int StudentRollNumber) {
+        this.StudentRollNumber = StudentRollNumber;
+    }
+
+}
---
+++ src/Student_Store_Record/src/service/StudentService.java
@@ -0,0 +1,15 @@
+package service;
+
+import java.util.List;
+
+import dto.Student;
+
+public  interface StudentService {
+    public void insert(Student student);
+    public void update(Student student);
+    public void delete(int id);
+    public Student getStudentById(int id);
+    public List<Student> getAll();
+    public Student getByEmailAndPassword(String email, String password);
+    
+}
