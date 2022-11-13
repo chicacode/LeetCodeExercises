8149cd6917095c5e886aaba31f75b7398bf52181
chicacode <gerahernan.al@gmail.com>
2022-11-12 19:40:48 -0800

    feat: added interface implementation

--- src/Student_Store_Record/src/service/StudentServiceIm.java
+++ src/Student_Store_Record/src/service/StudentServiceIm.java
@@ -2,37 +2,41 @@ package service;
 
 import java.util.List;
 import dto.Student;
+import dao.StudentDAO;
+import dao.StudentMySQLIm;
 
-public class StudentServiceIm implements StudentService{
+public class StudentServiceIm implements StudentService {
+
+    private StudentDAO studentDAO = new StudentMySQLIm();
 
     @Override
-    public void insert(Student student){
-
+    public void insert(Student student) {
+        studentDAO.insert(student);
     }
 
     @Override
-    public void update(Student student){
-
+    public void update(Student student) {
+        studentDAO.update(student);
     }
 
     @Override
-    public void delete(int id){
-
+    public void delete(int id) {
+        studentDAO.delete(id);
     }
 
     @Override
-    public Student getStudentById(int id){
-        return "";
+    public Student getStudentById(int id) {
+        return studentDAO.getStudentById(id);
     }
 
     @Override
-    public List<Student> getAll(){
-        return "";
+    public List<Student> getAll() {
+        return studentDAO.getAll();
     }
 
     @Override
-    public Student getByEmailAndPassword(String email, String password){
-        return "";
+    public Student getByEmailAndRollNumber(String email, int StudentRollNumber) {
+        return studentDAO.getByEmailAndRollNumber(email, 0);
     }
-    
+
 }
