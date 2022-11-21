28ceded96b8e7cc4ba0e56bd83a876bdad1d9e3f
chicacode <gerahernan.al@gmail.com>
2022-11-20 18:17:57 -0800

    feat: added update and delete method

--- src/Student_Store_Record/src/App.java
+++ src/Student_Store_Record/src/App.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 import dto.Student;
@@ -21,13 +22,10 @@ public class App {
             System.out.println("----------------------");
             System.out.println(" Welcome to CICCC STUDENT RECORD");
             System.out.println("1. - for REGISTER");
-            System.out.println("2. - for LOGIN");
-            System.out.println("3. - for GET ALL STUDENTS");
-            System.out.println("4. - for GET STUDENT BY ID");
-            System.out.println("5. - for UPDATE STUDENT");
-            System.out.println("6. - for DELETE STUDENT");
-            System.out.println("7. - for GET STUDENT BY ROLL NUMBER AND EMAIL");
-            System.out.println("8. - Q for QUIT");
+            System.out.println("2. - for UPDATE STUDENT");
+            System.out.println("3. - for DELETE STUDENT");
+            System.out.println("4. - for GET STUDENT BY ROLL NUMBER AND EMAIL");
+            System.out.println("5. - Q for QUIT");
             System.out.println("----------------------");
 
             userChoice = scanner.nextLine();
@@ -41,9 +39,9 @@ public class App {
                     System.out.println("- Enter your email -");
                     String email = scanner.nextLine();
                     System.out.println("- Enter your roll number -");
-                    int StudentRollNumber = scanner.nextInt();
+                    int studentRollNumber = scanner.nextInt();
 
-                    Student student = new Student(name, email, studentId, StudentRollNumber);
+                    Student student = new Student(name, email, studentId, studentRollNumber);
                     studentService.insert(student);
 
                     break;
@@ -51,11 +49,44 @@ public class App {
                 case "2":
 
                     System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+                    displayStudent(studentService.getAll());
+                    System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
+                    System.out.println("Enter student ID");
+                    int id = scanner.nextInt();
+
+                    Student student1 = studentService.getStudentById(id);
+
+                    if (student1 != null) {
+                        System.out.println("- Enter new name -");
+                        String newName = scanner.nextLine();
+                        System.out.println("- Enter new studentId -");
+                        String newStudentId = scanner.nextLine();
+                        System.out.println("- Enter new email -");
+                        String newEmail = scanner.nextLine();
+                        System.out.println("- Enter new roll number -");
+                        int newStudentRollNumber = scanner.nextInt();
+
+                        student1.setName(newName);
+                        student1.setStudentId(newStudentId);
+                        student1.setEmail(newEmail);
+                        student1.setStudentRollNumber(newStudentRollNumber);
+
+                        studentService.update(student1);
+                    } else {
+                        System.out.println("- Student Not Found -");
+                    }
                     break;
 
                 case "3":
+                    System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+                    displayStudent(studentService.getAll());
+                    System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
+                    System.out.println("Enter student ID");
+                    id = scanner.nextInt();
+
+                    studentService.delete(id);
                     break;
 
                 case "4":
@@ -66,21 +97,21 @@ public class App {
 
                     break;
 
-                case "6":
-
-                    break;
-
-                case "7":
-
-                    break;
-
-                case "8":
-
-                    break;
             }
 
-        } while (!userChoice.equals("8"));
+        } while (!userChoice.equals("5"));
 
         scanner.close();
+    }
+
+    public static void displayStudent(List<Student> students) {
+        System.out.println("Student data: ID\tName\tEmail\tStudentId\tRollNumber");
+        for (Student student : students) {
+            System.out.print(student.getName() + " \t");
+            System.out.print(student.getEmail() + " \t");
+            System.out.print(student.getStudentId() + " \t");
+            System.out.print(student.getStudentRollNumber() + " \t");
+            System.out.println("\n");
+        }
     }
 }
